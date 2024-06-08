4c302dfa6555e6d0c738538c16daf9e6e8666966
Jhon-crypt <johnoladele690@gmail.com>
2024-06-08 18:23:27 +0100

    feat: Added sections

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -19,7 +19,7 @@ export default function Header() {
     return (
         <nav className="d-flex align-content-center navbar navbar-expand-lg navbar-light bg-white shadow px-3 py-1">
             <a className="navbar-brand" href="#">
-                <img src="/logo.png" className="d-inline-block align-top" alt="Logo" />
+                <img src="logo.png" className="d-inline-block align-top" alt="Logo" />
             </a>
 
             <div className="collapse navbar-collapse justify-content-center tab-switch mb-3">
